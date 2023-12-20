6b95b19cb7ebb1c2f376d5e9d91b0b575afb9b4a
Charlie <charlieedoherty@gmail.com>
2023-12-20 00:56:45 -0600

    Add functionality through zipper.py, change front-end, and add documentation.

--- FileZipper/main.py
+++ FileZipper/main.py
@@ -1,17 +1,33 @@
 import PySimpleGUI as SimpGUI
+from zipper import make_archive
 
-
-c_label = SimpGUI.Text("Select files to compress: ")
+# Front-end
+c_label = SimpGUI.Text("Select files to compress: ", background_color="white", text_color="black")
 c_input = SimpGUI.Input()
-c_button = SimpGUI.FilesBrowse("Choose")
+c_button = SimpGUI.FilesBrowse("Choose", key="files")
 
-d_label = SimpGUI.Text("Select destination folder: ")
+d_label = SimpGUI.Text("Select destination folder: ", background_color="white", text_color="black")
 d_input = SimpGUI.Input()
-d_button = SimpGUI.FolderBrowse("Choose")
+d_button = SimpGUI.FolderBrowse("Choose", key="folder")
 
 run_button = SimpGUI.Button("Compress")
+output_label = SimpGUI.Text(key="output", text_color="green", background_color="white")
 
+window = SimpGUI.Window("File Zipper", background_color="white", layout=[[c_label, c_input, c_button],
+                                                                         [d_label, d_input, d_button],
+                                                                         [run_button, output_label]])
 
-window = SimpGUI.Window("File Zipper", layout=[[c_label, c_input, c_button], [d_label, d_input, d_button], [run_button]])
-window.read()
+# Back-end
+while True:
+    event, values = window.read()
+
+    filepaths = values[0].split(";")
+    folder = values[1]
+
+    if event == SimpGUI.WIN_CLOSED:
+        break
+    elif event == "Compress":
+        make_archive(filepaths, folder)
+        window["output"].update("Compression successful!")
+
 window.close()
